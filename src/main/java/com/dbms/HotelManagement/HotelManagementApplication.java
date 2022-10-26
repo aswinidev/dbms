6b2531b16a7bca318e1a47db782ae8b16b38b604
P Aswini Kumar <aswini123p@gmail.com>
2022-10-26 21:03:24 +0530

    register

--- src/main/java/com/dbms/HotelManagement/HotelManagementApplication.java
+++ src/main/java/com/dbms/HotelManagement/HotelManagementApplication.java
@@ -2,6 +2,9 @@ package com.dbms.HotelManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HotelManagementApplication {
@@ -10,4 +13,13 @@ public class HotelManagementApplication {
 		SpringApplication.run(HotelManagementApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer configure(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
---
+++ src/main/java/com/dbms/HotelManagement/controller/RegisterController.java
@@ -0,0 +1,35 @@
+package com.dbms.HotelManagement.controller;
+
+import com.dbms.HotelManagement.model.User;
+import com.dbms.HotelManagement.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class RegisterController {
+    private final AuthenticationService authenticationService;
+
+    @Autowired
+    public RegisterController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public String submitRegister(@RequestBody User user) {
+        String fname = user.getFname();
+        String lname = user.getLname();
+        String pEmail = user.getpEmail();
+        String pswd = user.getPswd();
+        String houseNo = user.getHouseNo();
+        String state = user.getState();
+        String city = user.getCity();
+        String country = user.getCountry();
+        String pinCode = user.getPinCode();
+        String gender = user.getGender();
+        authenticationService.register(fname, lname, pEmail, pswd, houseNo, state, city, country, pinCode, gender);
+        return fname + " " + lname + " " + pEmail + " " + pswd + " " + houseNo + " " + state + " " + city + " " + country + " " + pinCode + " " + gender;
+    }
+}
