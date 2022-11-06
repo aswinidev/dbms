254d0124e7bd0890943f3948ceb834bbb943754f
tulshidas-avhad-consultancy <105477466+tulshidas-avhad-consultancy@users.noreply.github.com>
2022-11-06 15:16:40 +0530

    subordinate controller

---
+++ src/main/java/com/dbms/HotelManagement/service/EmployeeService.java
@@ -0,0 +1,28 @@
+package com.dbms.HotelManagement.service;
+
+import com.dbms.HotelManagement.model.Employee;
+import com.dbms.HotelManagement.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getEmployees() {
+        return employeeRepository.getAllEmployee();
+    }
+
+    public List<Employee> getSubord(UUID empID) {
+        return employeeRepository.getSubord(empID);
+    }
+}
