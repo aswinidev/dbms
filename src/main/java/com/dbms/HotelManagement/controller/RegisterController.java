1cf82eb831b804ba797ec2ef992d45e005814f01
P Aswini Kumar <aswini123p@gmail.com>
2022-11-12 15:01:36 +0530

    links error resolved4

--- src/main/java/com/dbms/HotelManagement/controller/RegisterController.java
+++ src/main/java/com/dbms/HotelManagement/controller/RegisterController.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "https://lelotusgrand.netlify.app")
+@RequestMapping("https://lelotusgrand.herokuapp.com")
 public class RegisterController {
     private final AuthenticationService authenticationService;
     private final PasswordEncoder passwordEncoder;
